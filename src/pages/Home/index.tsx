705c5a652103c8cdbbc3f4f457fd1240d3b0bebc
ebertmota <ebert.mota03@gmail.com>
2021-01-21 22:33:51 -0300

    testing new countdown timer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { clear } from 'console';
+import { parse } from 'path';
+import React, { useState, useEffect, useCallback } from 'react';
 import Header from '../../components/Header';
 
 import {
@@ -10,14 +12,64 @@ import {
 } from './styles';
 
 const Home: React.FC = () => {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [started, setIsStarted] = useState(false);
+  const [running, setIsRunnig] = useState(false);
+
+  useEffect(() => {
+    if (running) {
+      // exit early when we reach 0
+      if (!seconds) {
+        if (!minutes) return;
+        const minutesInterval = setInterval(() => {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }, 1000);
+
+        // clear interval on re-render to avoid memory leaks
+        return () => clearInterval(minutesInterval);
+        // add timeLeft as a dependency to re-rerun the effect
+        // when we update it
+      }
+
+      // save intervalId to clear the interval when the
+      // component re-renders
+      const secondsInterval = setInterval(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+
+      // clear interval on re-render to avoid memory leaks
+      return () => clearInterval(secondsInterval);
+      // add timeLeft as a dependency to re-rerun the effect
+      // when we update it
+    }
+  }, [seconds, running, minutes]);
+
+  const controlsCountdown = () => {
+    if (started === false) {
+      setMinutes(state => state - 1);
+      setSeconds(59);
+      setIsStarted(true);
+    }
+    setIsRunnig(state => !state);
+  };
+
   return (
     <Container>
       <Header />
 
       <Content>
         <Timer>
-          <span>25:00</span>
-          <button type="button">START</button>
+          <span>
+            {String(minutes).padStart(2, '0')}:
+            {String(seconds).padStart(2, '0')}
+          </span>
+          <button type="button" onClick={controlsCountdown}>
+            {running || (started === false && 'START')}
+            {running && 'PAUSE'}
+            {running === false && seconds < 60 && seconds > 0 && 'RESUME'}
+          </button>
         </Timer>
       </Content>
       <DetailsSection>
